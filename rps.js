a911ecc3aa29587d6f48e472b08e0822605b65d1
Blade Barringer <blade@crookedneighbor.com>
2015-02-12 20:00:02 -0600

    Adjusted spacing for rps and changed return to console.log

--- rps.js
+++ rps.js
@@ -6,34 +6,38 @@ if (computerChoice < 0.34) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
+} 
+
+console.log("Computer: " + computerChoice);
 
 var compare = function(choice1, choice2){
-    if (choice1 === choice2){
-        return "The result is a tie!";
+  if (choice1 == choice2){
+    console.log("The result is a tie!");
+  }
+  else if (choice1 == "rock"){
+    if (choice2 == "scissors"){
+      console.log("rock wins")
     }
-    else if (choice1 === "rock"){
-        if (choice2 === "scissors"){
-            console.log("rock wins")
-        }
-        else {
-            console.log("paper wins")
-        }
+    else {
+      console.log("paper wins")
     }
-    else if (choice1 === "paper"){
-        if (choice2 === "rock"){
-            console.log("paper wins")
-        }
-        else {
-            console.log("scissors wins")
-        }
+  }
+  else if (choice1 == "paper"){
+    if (choice2 == "rock"){
+      console.log("paper wins")
     }
-    else{
-        if (choice2 ==="paper"){
-            console.log("scissors wins")
-        }
-        else {
-            console.log("rock wins")
-        }
+    else {
+      console.log("scissors wins")
     }
+  }
+  else{
+    if (choice2 =="paper"){
+      console.log("scissors wins")
+    }
+    else {
+      console.log("rock wins")
+    }
+  }
 }
+
+compare(userChoice, computerChoice);
